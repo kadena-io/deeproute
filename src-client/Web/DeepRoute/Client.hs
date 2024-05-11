1a78592c4f04ec180532b77b421cad2d8f3660e2
Edmund Noble <edmundnoble@gmail.com>
2024-05-11 17:51:01 -0400

    Massive change of extension points
    Put a parser into ApiRequest, but don't use it in doRequestUnchecked

--- src-client/Web/DeepRoute/Client.hs
+++ src-client/Web/DeepRoute/Client.hs
@@ -1,12 +1,45 @@
 {-# language OverloadedStrings #-}
 {-# language TemplateHaskell #-}
+{-# language DeriveFunctor #-}
+{-# language LambdaCase #-}
+{-# language RankNTypes #-}
+{-# language FunctionalDependencies #-}
+{-# language FlexibleInstances #-}
 
-module Web.DeepRoute.Client where
+module Web.DeepRoute.Client
+    ( bodyReaderToLazyByteString
+    , jsonBody
+    , bytesBody
+    , ClientEnv(..)
+    , ClientError(..)
+    , doRequestParsed
+    , doRequestEither
+    , doRequestThrow
+    , doRequestUnchecked
+    , defaultResponseChecks
+    , (/@)
+    , (/@@)
 
-import Control.Exception
+    , ApiRequest(..)
+    , mkApiRequest
+    , requestPath
+    , requestQuery
+    , requestAcceptable
+    , requestHeaders
+    , requestBody
+    , requestMethod
+    , requestModifier
+    , requestResponseParser
+
+    , checkAcceptable
+    , checkSuccessful
+    ) where
+
+import Control.Exception (evaluate)
 import Control.Lens
-import Control.Monad
+import Control.Monad.IO.Class
 import Data.Aeson
+import Data.Bitraversable
 import Data.ByteString(ByteString)
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Builder as BSB
@@ -24,19 +57,32 @@ import Network.HTTP.Media
 import Network.HTTP.Types
 import Web.HttpApiData
 import Network.HTTP.Client (Response(responseBody))
+import Control.Exception.Safe
+import Control.Monad.Except
+import Data.Void (Void)
+import Data.Traversable (for)
+import Data.ByteString.Lazy (LazyByteString)
 
-readLazyResponseBody :: (LBS.ByteString -> IO r) -> Client.Response Client.BodyReader -> IO r
-readLazyResponseBody kont resp = do
-    bodyBytes <- brLazy (Client.responseBody resp)
-    kont bodyBytes
+-- morally just converts a BodyReader into a lazy bytestring faithfully, i.e.
+-- without buffering
+bodyReaderToLazyByteString :: Client.BodyReader -> IO LBS.ByteString
+bodyReaderToLazyByteString reader = do
+    brLazy reader
     where
     brLazy br = unsafeInterleaveIO $ do
         next <- Client.brRead br
         if BS.null next then return mempty
         else LBS.chunk next <$> brLazy br
 
-readJsonResponseBody :: FromJSON a => Client.Response Client.BodyReader -> IO (Maybe a)
-readJsonResponseBody = readLazyResponseBody (evaluate . decode')
+-- this isn't lazy really, but it does at least interleave response body
+-- consumption with parsing.
+jsonBody :: FromJSON a => LazyByteString -> IO (Either AesonException a)
+jsonBody bytes =
+    bitraverse evaluate evaluate $
+        over _Left AesonException (eitherDecode' bytes)
+
+bytesBody :: LazyByteString -> IO ByteString
+bytesBody = evaluate . LBS.toStrict
 
 data ClientEnv
     = ClientEnv
@@ -47,35 +93,56 @@ data ClientEnv
     , _clientEnvRequestModifier :: !(Client.Request -> Client.Request)
     }
 
-data ClientError
-    = UnacceptableResponse !MediaType
+data ClientError e
+    = UnacceptableResponse !ByteString
+    -- ^ the content type header's contents
     | UnsuccessfulStatus !Status
+    | UserClientError e
+    | ClientHTTPException !Client.HttpException
     deriving Show
 
-instance Exception ClientError
+instance (Show e, Typeable e) => Exception (ClientError e)
 
-makeClassy ''ClientEnv
-
-data ApiRequest
+data ApiRequest a
     = ApiRequest
     { _requestPath :: BSB.Builder
     -- the user fills this out directly, using the toQueryParam function
     , _requestQuery :: !QueryText
     , _requestAcceptable :: !(Maybe [Quality MediaType])
-    , _requestSuccessful :: !(Status -> Bool)
     , _requestHeaders :: !RequestHeaders
     , _requestBody :: !Client.RequestBody
     , _requestMethod :: !Method
     , _requestModifier :: !(Client.Request -> Client.Request)
+    , _requestResponseParser :: !(Client.Response LBS.ByteString -> IO (Client.Response a))
     }
-
-debugPrintApiRequest (ApiRequest p q a _ h _ m _) = unwords
-    ["ApiRequest", show (BSB.toLazyByteString p), show q, show a, show h, show m]
+    deriving (Functor)
 
 makeLenses ''ApiRequest
 
-doRequest :: ClientEnv -> ApiRequest -> (Client.Response Client.BodyReader -> IO a) -> IO a
-doRequest env req kont = do
+makeClassy ''ClientEnv
+
+mkApiRequest
+    :: Method
+    -> (Client.Response LBS.ByteString -> IO (Client.Response a))
+    -> BSB.Builder
+    -> ApiRequest a
+mkApiRequest method parser path = ApiRequest
+    { _requestPath = path
+    , _requestMethod = method
+    , _requestQuery = []
+    , _requestAcceptable = Nothing
+    , _requestHeaders = []
+    , _requestBody = Client.RequestBodyBS mempty
+    , _requestModifier = id
+    , _requestResponseParser = parser
+    }
+
+debugPrintApiRequest :: ApiRequest a -> String
+debugPrintApiRequest (ApiRequest p q a h _ m _ _) = unwords
+    ["ApiRequest", show (BSB.toLazyByteString p), show q, show a, show h, show m]
+
+doRequestParsed :: ClientEnv -> ApiRequest a -> (Client.Response a -> IO r) -> IO r
+doRequestParsed env req kont = do
     let acceptHeader = maybe [] (\ts -> [("Accept", renderHeader ts)]) (_requestAcceptable req)
     let req' =
             Client.defaultRequest
@@ -87,55 +154,67 @@ doRequest env req kont = do
                 , Client.queryString = LBS.toStrict $ BSB.toLazyByteString $ renderQueryText True (_requestQuery req)
                 , Client.requestHeaders = acceptHeader <> _requestHeaders req
                 , Client.requestBody = _requestBody req
-                , Client.checkResponse = \_ resp ->
-                    let status = Client.responseStatus resp
-                    in unless (_requestSuccessful req status) $ throwIO $ UnsuccessfulStatus status
                 }
     let req'' = _requestModifier req (_clientEnvRequestModifier env req')
     Client.withResponse req'' (_clientEnvManager env) $ \resp -> do
-        let responseContentTypeHeader = parseAccept =<< lookup "Content-Type" (Client.responseHeaders resp)
-        case (_requestAcceptable req, responseContentTypeHeader) of
-            (Just acceptables, Just responseContentType)
-                -- here we do our own "content type negotiation" with the only option
-                -- being the actually-returned content type, to determine if the response
-                -- content type was acceptable.
-                | Nothing <- matchQuality [responseContentType] acceptables ->
-                    throwIO (UnacceptableResponse responseContentType)
-            _ ->
-                kont resp
-{-# inlinable doRequest #-}
+        resp' <- traverse bodyReaderToLazyByteString resp
+        kont =<<
+            _requestResponseParser req resp'
+{-# inlinable doRequestParsed #-}
 
-doRequestForEffect :: ClientEnv -> ApiRequest -> IO ()
-doRequestForEffect env req = doRequest env req (const (return ()))
-{-# inline conlike doRequestForEffect #-}
+checkAcceptable :: ApiRequest a -> Client.Response r -> ExceptT (ClientError e) IO ()
+checkAcceptable req resp
+    | Just acceptables <- _requestAcceptable req
+    , Just responseContentTypeHeader <- lookup "Content-Type" (Client.responseHeaders resp) =
+        case parseAccept responseContentTypeHeader of
+            Just responseContentType
+                | Just _ <- matchQuality [responseContentType] acceptables
+                    -> return ()
+            _ -> throwError (UnacceptableResponse responseContentTypeHeader)
+    | otherwise = return ()
 
-doJSONRequest :: FromJSON a => ClientEnv -> ApiRequest -> IO (Client.Response a)
-doJSONRequest env req = do
-    resp <- doJSONRequest' env req
-    body' <- evaluate $ fromMaybe (error "invalid json in response") $ responseBody resp
-    return resp { responseBody = body' }
-{-# inlinable doJSONRequest #-}
+checkSuccessful :: Client.Response r -> ExceptT (ClientError e) IO ()
+checkSuccessful resp
+    | statusIsSuccessful (Client.responseStatus resp) = return ()
+    | otherwise = throwError (UnsuccessfulStatus (Client.responseStatus resp))
 
-doJSONRequest' :: FromJSON a => ClientEnv -> ApiRequest -> IO (Client.Response (Maybe a))
-doJSONRequest' env req =
-    doRequest env req $ \resp -> do
-        json <- readJsonResponseBody resp
-        return resp { responseBody = json }
-{-# inline conlike doJSONRequest' #-}
+defaultResponseChecks
+    :: ApiRequest a -> Client.Response r -> ExceptT (ClientError e) IO ()
+defaultResponseChecks req resp = do
+    -- check success first, many servers don't want to give acceptable
+    -- content types in error situations
+    checkSuccessful resp
+    checkAcceptable req resp
+{-# inline defaultResponseChecks #-}
 
-infixl 2 `withMethod`
-withMethod :: BSB.Builder -> Method -> ApiRequest
-withMethod r m = ApiRequest
-    { _requestPath = r
-    , _requestQuery = []
-    , _requestAcceptable = Nothing
-    , _requestSuccessful = statusIsSuccessful
-    , _requestHeaders = []
-    , _requestBody = Client.RequestBodyBS mempty
-    , _requestMethod = m
-    , _requestModifier = id
-    }
-{-# inline conlike withMethod #-}
+doRequestEither
+    :: ClientEnv
+    -> ApiRequest (Either e a)
+    -> IO (Either (ClientError e) (Client.Response a))
+doRequestEither env req = doRequestParsed env req $ \resp ->
+    runExceptT $ do
+        defaultResponseChecks req resp
+        traverse (either (throwError . UserClientError) return) resp
+{-# inlinable doRequestEither #-}
+
+doRequestThrow
+    :: (Typeable e, Show e)
+    => ClientEnv
+    -> ApiRequest (Either e a)
+    -> IO (Client.Response a)
+doRequestThrow env req =
+    either throwIO return =<<
+        doRequestEither env req
+{-# inlinable doRequestThrow #-}
+
+-- Careful! Status and returned content type both go unchecked.
+doRequestUnchecked
+    :: ClientEnv
+    -> ApiRequest a
+    -> IO (Client.Response ByteString)
+doRequestUnchecked env req =
+    doRequestParsed env req { _requestResponseParser = traverse bytesBody }  return
+{-# inlinable doRequestUnchecked #-}
 
 infixl 3 /@
 (/@) :: BSB.Builder -> ByteString -> BSB.Builder
